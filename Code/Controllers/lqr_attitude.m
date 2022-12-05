8d63c87ff5cf68621b675fd438c1bcfa758c824f
Heisenberg-kd <kartikaya2000@gmail.com>
2022-12-06 02:52:17 +0530

    Attitude Control Patch-5 [BTP Done]

--- Code/Controllers/lqr_attitude.m
+++ Code/Controllers/lqr_attitude.m
@@ -10,11 +10,10 @@ R = eye(3);
 K = lqr(A,B,Q,R);
 
 %% Simulate closed-loop system
-% x0 = [-1000;-200;-4000;30;40;50];   % initial condition 
-% xdes = @(t)[0; 0; 0; sin(pi*t/32); 0; cos(pi*t/32)];      % reference position  [w  q]
+xref=[0; 0; 0;0*sin(n*t); 0; 0*cos(n*t)];
 u=@(x,t)-K*(x - [0; 0; 0;0*sin(n*t); 0; 0*cos(n*t)]);                % control law
-
 [t,x] = ode45(@(t,x)diff_equ(x,u(x,t),A,B),tspan,x0);
+
 q0=(ones(size(t))-x(:,4).^2-x(:,5).^2-x(:,6).^2).^0.5;
 x=[x q0];
 x=x';
